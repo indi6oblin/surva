dec3f995f2942ec0709c5cad62375d7cc42304b4
4ldorizki <4ldorizki>
2024-06-03 15:35:24 +0700

    Admin Full

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -33,5 +33,41 @@ class AdminSeeder extends Seeder
                 'password' => Hash::make('Absar12345'),
             ]
         );
+        Admin::firstOrCreate(
+            [
+                'username' => 'aldo',
+                'email' => 'Aldo@gmail.com',
+            ],
+            [
+                'password' => Hash::make('Aldo12345'),
+            ]
+        );
+        Admin::firstOrCreate(
+            [
+                'username' => 'ririn',
+                'email' => 'ririn@gmail.com',
+            ],
+            [
+                'password' => Hash::make('Ririn12345'),
+            ]
+        );
+        Admin::firstOrCreate(
+            [
+                'username' => 'tyas',
+                'email' => 'tyas@gmail.com',
+            ],
+            [
+                'password' => Hash::make('Tyas12345'),
+            ]
+        );
+        Admin::firstOrCreate(
+            [
+                'username' => 'bowen',
+                'email' => 'tyas@gmail.com',
+            ],
+            [
+                'password' => Hash::make('Bowen12345'),
+            ]
+        );
     }
 }
