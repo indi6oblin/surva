b55985f84fb4fdbfa9b512071379d792a87fc797
4ldorizki <4ldorizki>
2024-06-06 15:08:09 +0700

    Surva

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APSIS</title>
+    <title>SURVA</title>
     <link rel="shortcut icon" href="{{ URL::to('assets/images/favicon.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.css') }}">
@@ -12,13 +12,13 @@
     <link rel="stylesheet" href="{{ URL::to('assets/css/app.css') }}">
     <link rel="stylesheet" href="{{ URL::to('assets/css/pages/auth.css') }}">
     {{-- message toastr --}}
-    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css"> 
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
     <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
     <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
 
     <link href="landingPage/assets/img/logoapp.png" rel="icon">
     <link href="landingPage/assets/img/logoapp.png" rel="apple-touch-icon">
-    
+
 </head>
 <style>
     .form-group[class*=has-icon-].has-icon-left .form-select {
